7696bde045a31d0aba459bdd1050f7ac4ca31277
0x67 <gabri.putra@gmail.com>
2024-12-10 16:09:58 +0700

    chore: fix unocss not applying config

--- uno.config.ts
+++ uno.config.ts
@@ -2,6 +2,6 @@ import {
   defineConfig,
 } from 'unocss'
 
-export default defineConfig({
-  configFile: './src/config/unocss.ts',
-})
+import { lithiumConfig } from './src/config/unocss'
+
+export default defineConfig(lithiumConfig)
